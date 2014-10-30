0905d2158fe6754ab05c5554ad40bd7d4a8ea9f4
johnmarinelli <jonmarinelli@hotmail.com>
2014-10-29 22:50:55 -0700

    added is_prime, need to test, for some reason array[] is getting cached in prime factors

--- algorithms/math/prime_factors.py
+++ algorithms/math/prime_factors.py
@@ -1,28 +1,51 @@
-import sieve_eratosthenes as sieve
+from math import sqrt
 
-def prime_factors(n, array=[], div=2, inc=0, primes_list=[]):
-  # check if we have a list of primes already.  this will be called at the first step
-  # jk this algorithm is slow as shit
-#  if not primes_list:
-#    primes_list = sieve.eratosthenes(n)
+#simple primality test from wikipedia for n > 5
+def is_prime(n):
+  #if n is less than or equal to 3, then return whether or not n is >= 2
+  if n <= 3:
+    return n >= 2
 
-  if div < 2: 
-    div = 2
+  #check if n is divisible by 2 or 3
+  if n % 2 == 0 or n % 3 == 0:
+    return False
 
-#  div = primes_list[inc]
-  print(n)
+  #finally, in the range of 5 to sqrt(n)+1 incrementing by 6,
+  #check if n is divisible by those primes or the odd numbers between those primes
+  for i in range(5, int(n ** 0.5) + 1, 6):
+    if n % i == 0 or n % (i+2) == 0:
+      return False
+
+  return True
+
+
+
+def prime_factors(n, array=[], div=1):
+  #make div a positive int
+  if div < 1: 
+    div = 1
+
+  inc = 1
+  #if div is > 2 we can increment by 2's, because no primes are even other than 2
+  if div > 2:
+    inc = 2
   
-  # if we're at the end point
-  if div >= n:
+  # if we're at the end point. divisors won't be larger than the square root of input
+  if div > n:
+    # if array length is still one, then it's a prime number
+    if len(array) == 0:
+      array.append(1)
+      array.append(n)
+
     return array
 
   # n is not a multiple of div, so proceed
   elif n % div != 0:
-    return prime_factors(n, array, div+1, inc+1, primes_list)
+    return prime_factors(n, array, div+inc)
 
-  # if n is a multiple of div, and it's not in the primes list, add it
+  # if n is a multiple of div, add it
   else:
     n = n / div
-    if div not in array:
+    if div not in array and is_prime(div):
       array.append(div)
-    return prime_factors(n, array, div+1, inc+1, primes_list)
+    return prime_factors(n, array, div+inc)
--- algorithms/math/totient.py
+++ algorithms/math/totient.py
@@ -1,5 +1,9 @@
 from decimal import Decimal
-from prime_factors import prime_factors as primeFactorization
+from prime_factors import prime_factors
+"""
+Euler's totient function:
+Counts the number of positive integers less than or equal to n that are relatively prime to n.
+"""
 
 def totient(n):
 	if n < 2:
@@ -7,19 +11,23 @@ def totient(n):
 
 	tnt = n
 	primeFactors = []
-	primeFactorization(n, primeFactors)
+	prime_factors(n, primeFactors)
 
+    #remove unnecessary values
 	if primeFactors.count(1) > 0:
 		primeFactors.remove(1)
 	
 	if primeFactors.count(n) > 0:
 		primeFactors.remove(n)
 
+    #remove duplicates
 	primeFactors = list(set(primeFactors))
-	
+    
+    #totient function is the product of (1-pn)(1-p(n-1))...(1-p(1)) times n
 	for i in primeFactors:
 		pf = Decimal(1 / Decimal(i))
 		a = Decimal(1 - pf)
 		tnt = Decimal(tnt * a)
+        print primeFactors
 
 	return int(tnt)
